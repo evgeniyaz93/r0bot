10a89ae5d4318212ead1a46cfbbae71e881eb81a
Zayn <dza9393@mail.ru>
2020-12-18 14:29:52 +0300

    done

--- src/main/java/ru/r0bot/R0botApplication.java
+++ src/main/java/ru/r0bot/R0botApplication.java
@@ -3,11 +3,13 @@ package ru.r0bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class R0botApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(R0botApplication.class, args);
     }
+
 
 }
